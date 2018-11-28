0c9d15f24aaf49dd6b4f1334ab9f37df2644af68
Dirk Dobson <dirkvancouver@live.com>
2018-11-27 17:21:33 -0700

    list methods

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace csLists
 {
@@ -6,7 +7,30 @@ namespace csLists
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numbers = new List<int>() { 1, 2, 3, 4, };
+            numbers.Add(1);
+            numbers.AddRange(new int[3] { 5, 6 ,7 });
+
+            foreach ( var i in numbers)
+                System.Console.WriteLine(i);
+
+            System.Console.WriteLine();
+            Console.WriteLine(numbers.IndexOf(1));
+            Console.WriteLine(numbers.LastIndexOf(1));
+
+            System.Console.WriteLine("Count" + numbers.Count);
+
+            
+            for( var i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == 1)
+                    numbers.Remove(numbers[i]);
+            }
+            foreach ( var i in numbers)
+                System.Console.WriteLine(i);
+
+            numbers.Clear();
+            System.Console.WriteLine("Count" + numbers.Count);
         }
     }
 }
